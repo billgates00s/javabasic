97fad760572023eacfa335151377aad916b318a4
billgates00s <nguyenhongnhana2@gmail.com>
2022-11-23 11:14:28 +0700

    Create MyFirst.java

---
+++ learnjava/src/main/java/lab1/lab1_1/MyFirst.java
@@ -0,0 +1,20 @@
+package lab1_1;
+
+/**
+ * @author nhannguyen
+ * This is my first java class,
+ * And in this class I am printing (displaying) a message on console
+ */
+public class MyFirst {
+    /**
+     * Main method - this is staring point of my program.
+     * When I run the program,
+     * JVM will look for this method and program   will start execution
+     */
+    public static void main(String[] args) {
+        // using System.out.println to print a message on console
+        System.out.println("Yeeeeee, My first java program. :-)");
+
+    }
+
+}
