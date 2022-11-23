34ab82713ff79e840354bd7ee48c4f3941b23ad1
billgates00s <nguyenhongnhana2@gmail.com>
2022-11-23 11:14:37 +0700

    Create MyFirstWithArgument.java

---
+++ learnjava/src/main/java/lab1/lab1_3/MyFirstWithArgument.java
@@ -0,0 +1,10 @@
+package lab1_3;
+
+/**
+ * @author nhannguyen
+ */
+public class MyFirstWithArgument {
+    public static void main(String[] args) {
+        System.out.println("Hi " +  args[0] + " " + args[1]);
+    }
+}
